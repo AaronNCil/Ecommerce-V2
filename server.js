958621c1a79d4e6f96a24231f4c208bfb8ea48a7
Aaron Cil <cilaaron27@gmail.com>
2022-12-29 22:36:54 -0500

    host

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 3000;
 
 const conn = mysql.createConnection({
     host: "127.0.0.1",
-    user: "ecom",
-    password: "Qweriopa1",
+    user: "root",
+    password: "password",
     database: "ecom"
 })
 
